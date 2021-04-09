9639fc196720904ad5b0cd1c93b3623f5b6adcda
Gabriel-R1507 <70273926+Gabriel-R1507@users.noreply.github.com>
2021-04-09 20:01:38 -0300

    Add files via upload

---
+++ MovieService/Data/SGCContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MovieService.Domain;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieService.Data
+{
+    public class SGCContext : DbContext
+    {
+        public DbSet<tbl_0001_user> tbl_0001_user { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer(config.GetConnectionString("SGCConnection"));
+        }
+    }
+}
---
+++ MovieService/Domain/tbl_0001_user.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieService.Domain
+{
+    public class tbl_0001_user
+    {
+        [Key]
+        public int cd_user { get; set; }
+        
+        [StringLength(200)]
+        public string email_user { get; set; }
+        
+        [StringLength(200)]
+        public string senha_user { get; set; }
+
+        [StringLength(100)]
+        public string nm_user { get; set; }
+        
+        [StringLength(20)]
+        public string dt_nasc { get; set; }
+
+        [StringLength(50)]
+        public string cidd_user { get; set; }
+
+        [StringLength(50)]
+        public string estd_user { get; set; }
+
+    }
+}
